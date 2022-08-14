c8d4db52f56b76a532659fe54920fc1268e6ef70
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2022-08-14 23:38:24 +0530

    Time: 214 ms (43.70%), Space: 16 MB (81.80%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,41 +1,6 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
-#         units = 0
-#         l, r = 0, len(height) - 1
-        
-#         # calc max right for every position
-#         maxright = []
-#         for i in range(len(height)):
-#             rightarr = height[i+1:]
-#             if not rightarr:
-#                 maxright.append(0)
-#             else:      
-#                 maxright.append(max(rightarr))
-            
-#         # calc max left for every position
-#         maxleft = []
-#         for i in range(1, len(height)+1):
-#             leftarr = height[:i-1]
-#             if not leftarr:
-#                 maxleft.append(0)
-#             else:
-#                 maxleft.append(max(leftarr))  
-                
-#         minLR = []
-        
-#         for i in range(len(maxleft)):
-#             minLR.append(min(maxleft[i], maxright[i]))
-            
-#         # now calculating water units
-        
-#         for i in range(len(height)):
-            
-#             units += (height[i] - minLR[i])
-            
-#         return units
-
-
         if not height: return 0
     
         units = 0
