b55a0f18bbdaffb9eb26e5a0cef8e7a4ed077cc7
Yuvraj Singh Pathania <58564635+batman004@users.noreply.github.com>
2022-09-18 17:14:01 +0530

    Time: 117 ms (95.27%), Space: 13.9 MB (75.91%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+        def countPallen(l,r):
+            local_cntr=0
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                local_cntr+=1
+                l-=1
+                r+=1
+            return local_cntr
+        
+        cnt = 0
+        
+        # odd length pallendromes
+        for i in range(len(s)):
+            
+            cnt += countPallen(i,i)
+            
+        # even length pallendromes
+        for i in range(len(s)-1):
+            
+            cnt += countPallen(i,i + 1)
+            
+               
+        return cnt
+        
